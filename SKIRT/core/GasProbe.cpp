5bc1db1eadf1dfdc62a450f7930866ae7639cd9c
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2020-03-16 19:16:17 +0100

    Run gas probe in parallel (extended diagnostics will be slow)

--- SKIRT/core/GasProbe.cpp
+++ SKIRT/core/GasProbe.cpp
@@ -4,9 +4,13 @@
 ///////////////////////////////////////////////////////////////// */
 
 #include "GasProbe.hpp"
+#include "FatalError.hpp"
 #include "Gas.hpp"
 #include "Log.hpp"
 #include "MediumSystem.hpp"
+#include "Parallel.hpp"
+#include "ParallelFactory.hpp"
+#include "ProcessManager.hpp"
 #include "TextOutFile.hpp"
 
 ////////////////////////////////////////////////////////////////////
@@ -16,22 +20,48 @@ void GasProbe::probeRun()
     auto ms = find<MediumSystem>(false);
     auto grid = ms->grid();
     auto log = find<Log>(false);
+
     if (!ms->hasGas())
     {
         log->warning("No gas is present! Gas probe will not run!");
         return;
     }
+
     TextOutFile file(this, "gas", "gas properties per cell");
+
+    // cell coordinates
     file.addColumn("index", "", 'd');
     for (auto s : {"x", "y", "z"}) file.addColumn(s, "m", 'e');
-    file.addColumn("T", "K", 'e');
-    for (auto s : {"np", "nH", "nH2"}) file.addColumn(s, "cm-3", 'e');
+    int numCols = 4;
 
+    // temperature
+    file.addColumn("T", "K", 'e');
+    numCols++;
+
+    // abundances
+    for (auto s : {"np", "nH", "nH2"}) file.addColumn(s, "cm-3", 'e');
+    numCols += 3;
+
+    // do the diagnostics calculation in parallel (mostly relevent for the extended option)
     int numCells = grid->numCells();
-    for (int m = 0; m < numCells; m++)
-    {
-        Position p = grid->centralPositionInCell(m);
-        file.writeRow(vector<double>{static_cast<double>(m), p.x(), p.y(), p.z(), Gas::temperature(m), Gas::np(m),
-                                     Gas::nH(m), Gas::nH2(m)});
-    }
+    Table<2> numbers(numCells, numCols);
+    find<ParallelFactory>()->parallelDistributed()->call(numCells, [&](size_t firstIndex, size_t numIndices) {
+        for (size_t m = firstIndex; m < firstIndex + numIndices; m++)
+        {
+            Position p = grid->centralPositionInCell(m);
+            vector<double> numbersForCell = {static_cast<double>(m), p.x(),      p.y(),      p.z(),
+                                             Gas::temperature(m),    Gas::np(m), Gas::nH(m), Gas::nH2(m)};
+            // temporary check for typo's
+            int numNumbers = numbersForCell.size();
+            if (numNumbers != numCols) FATALERROR("Incorrect number of elements for row");
+
+            std::copy(std::begin(numbersForCell), std::end(numbersForCell), &numbers(m, 0));
+        }
+    });
+
+    // write (root only)
+    ProcessManager::sumToRoot(numbers.data());
+    for (int m = 0; m < numCells; m++) file.writeRow(vector<double>(&numbers(m, 0), &numbers(m, numCols)));
 }
+
+////////////////////////////////////////////////////////////////////
